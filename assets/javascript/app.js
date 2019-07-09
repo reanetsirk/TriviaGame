bd8639115cd5e78542cb56e0e8901de4a0faf56c
reanetsirk <reanetsirk@gmail.com>
2019-07-08 23:04:15 -0400

    trying

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,26 +3,27 @@ $(document).ready(function () {
 
     // variables
     // start time, time remaining, questions, answers, correct answer, wins, losses, restart
-    var wins = 0;
-    var losses = 0;
+    var currentQuestion = 0;   
+    var correct = 0;
+    var incorrect = 0;
     var unanswered = 0;
     var timeRemaining = 30;
     var triviaQuestions = {
-        question1 = {
+        question1 : {
             question: "Sesame Street was first aired on public braodcasting televsion on what date?",
             answerList: ["November 10, 1969","October 5, 1989", "March 27, 1954,"],
             answer: 0,
             answerText: "Sesame Street was first aired on public braodcasting televsion on Novemeber 10, 1969."
         },
         
-         question2 = {
+         question2 : {
          question: "True or False: The cast of Sesame Street consist of puppets and human characters.",
          answerList: ["True","False"],
          answer: 0,
          answerText: "The cast of Sesame Street consist of puppets and human characters. True."
          },
          
-         question3 = {
+         question3 : {
              question: "Who was the main protagonist of the series and the first Muppet to appear on the show?",
              answerList: ["Oscar the gouch","Big Bird","Elmo"],
              answer: 1,
@@ -30,12 +31,47 @@ $(document).ready(function () {
          },
     }
 
-    $("#startBtn").on("click", function () {
+    $('#startBtn').on('click', function () {
     $(this).hide();
     newGame();
     });
 
 
+    $('#startOverBtn').on('click', function (){
+        $(this).hide();
+        newGame();
+    });
+function newGame(){
+$("#game").show();
+$("#correct").empty();
+$("#incorrect").empty();
+$("#unanswered").empty();
+$("#gif").hide();
+$("#gifCaption").hide();
+currentQuestion
+correct
+incorrect
+unanswered
+newQuestion();
+
+}
+function newQuestion(){
+    $("#correct").empty();
+    $("#gif").hide();
+    $("#gifCaption").hide();
+    answer = true;
+
+}
+    $("#correct").html("Question" + (correct+1) + "of" + triviaQuestions.length);
+  $(".question").html("<h2>" + triviaQuestions[currentQuestion].question + "</h2>");
+
+  for(var i = 0; i < 2; i++) {
+var choices = $("<div>");
+choices.text(triviaQuestions[currentQuestion].answerList[i]);
+		choices.attr({'data-index': i });
+		choices.addClass('thisChoice');
+		$('.answerList').append(choices);
+  }
     
 });
 
