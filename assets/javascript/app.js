ee7e865fd4ed0c039de19409647c56d4b853971d
reanetsirk <reanetsirk@gmail.com>
2019-07-16 19:53:34 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,91 +1,179 @@
-
-$(document).ready(function () {
-
-    // variables
-    // start time, time remaining, questions, answers, correct answer, wins, losses, restart
-    var currentQuestion = 0;   
-    var correct = 0;
-    var incorrect = 0;
-    var unanswered = 0;
-    var timeRemaining = 30;
-    var gifArray = ["question1", "question2", "question3"],
-    var message = {
-        correct: "Correct Answer!",
-        incorrect: "Incorrect Answer!",
-        endTime: "Times Up!",
-        finished: "Your Results",
-    }
-    var triviaQuestions = {
-        question1 : {
-            question: "Sesame Street was first aired on public braodcasting televsion on what date?",
-            answerList: ["November 10, 1969","October 5, 1989", "March 27, 1954,"],
-            answer: 0,
-            answerText: "Sesame Street was first aired on public braodcasting televsion on Novemeber 10, 1969."
-        },
-        
-         question2 : {
-         question: "True or False: The cast of Sesame Street consist of puppets and human characters.",
-         answerList: ["True","False"],
-         answer: 0,
-         answerText: "The cast of Sesame Street consist of puppets and human characters. True."
-         },
-         
-         question3 : {
-             question: "Who was the main protagonist of the series and the first Muppet to appear on the show?",
-             answerList: ["Oscar the gouch","Big Bird","Elmo"],
-             answer: 1,
-             answerText: "Big Bird was the main protagonist of the series and the first Muppet to appear on the show."
-         },
-    }
-
-    $('#startBtn').on('click', function () {
-    $(this).hide();
-    newGame();
-    });
-
-
-    $('#startOverBtn').on('click', function (){
-        $(this).hide();
-        newGame();
-    });
-function newGame(){
-$("#game").show();
-$("#correct").empty();
-$("#incorrect").empty();
-$("#unanswered").empty();
-$("#gif").hide();
-$("#gifCaption").hide();
-currentQuestion
-correct
-incorrect
-unanswered
-newQuestion();
-
-}
-function newQuestion(){
-    $("#message").empty();
-    $("#correct").empty();
-    $("#gif").hide();
-    $("#gifCaption").hide();
-    answer = true;
-
-}
-    $("#correct").html("Question" + (correct+1) + "of" + triviaQuestions.length);
-  $(".question").html("<h2>" + triviaQuestions[currentQuestion].question + "</h2>");
-
-  for(var i = 0; i < 2; i++) {
-var choices = $("<div>");
-choices.text(triviaQuestions[currentQuestion].answerList[i]);
-		choices.attr({'data-index': i });
-		choices.addClass('thisChoice');
-		$('.answerList').append(choices);
-  }
-    
+$('#startBtn').on('click', function () {
+    game.start();
+})
+$(document).on('click','#end',function(){
+    game.done();
 });
 
+var triviaQuestions = [{
+        question: "Sesame Street was first aired on public braodcasting televsion on what date?",
+        answerList: ["November 10, 1969", "October 5, 1989", "March 27, 1954,"],
+        correctAnswer: 0,
+        answerText: "Sesame Street was first aired on public braodcasting televsion on Novemeber 10, 1969."
+    },
+    {
+        question: "True or False: The cast of Sesame Street consist of puppets and human characters.",
+        answerList: ["True", "False"],
+        correctAnswer: 0,
+        answerText: "The cast of Sesame Street consist of puppets and human characters. True."
+    },
+
+    {
+        question: "Who was the main protagonist of the series and the first Muppet to appear on the show?",
+        answerList: ["Oscar the gouch", "Big Bird", "Elmo"],
+        correctAnswer: 1,
+        answerText: "Big Bird was the main protagonist of the series and the first Muppet to appear on the show."
+    }
+];
+
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 120,
+    countdown: function () {
+        game.counter;
+        $('#counter').html(game.counter);
+        if (game.counter <= 0) {
+            console.log("Your time is up");
+            game.done();
+        }
+    },
+    start: function () {
+        timer = setInterval(game.countdown, 1000);
+        $('#subWrapper').prepend('<h2>Time Left: <span id= "counter" >120</span> Seconds</h2>');
+
+        $('#startBtn').remove();
+        for (var i = 0; i < triviaQuestions.length; i++) {
+            $('#subWrapper').append(triviaQuestions[i].question);
+            for (var j = 0; j < triviaQuestions[i].answerList.length; j++) {
+                $("#subWrapper").append("<input type='radio' name='question-" + i + " 'value='" + triviaQuestions[i].answerList[j] + "'>" + triviaQuestions[i].answerList[j]);
+            }
+        }
+$('#subWrapper').append('<br><button id="end">Done</button>');
+    },
+    done: function () {
+        $.each($('input[name="question-0]":checked'), function () {
+            if ($(this).val() == triviaQuestions[0].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-1]":checked'), function () {
+            if ($(this).val() == triviaQuestions[1].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-2]":checked'), function () {
+            if ($(this).val() == triviaQuestions[2].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        this.result();
+    },
+    result: function(){
+        clearInterval(timer);
+        $('#subWrapper h2').remove();
+
+        $('#subWrapper').html("<h2>All Finished</h2>");
+        $('#subWrapper').append("<h3>Correct:"+this.correct+"</h3>");
+        $('#subWrapper').append("<h3>Incorrect:"+this.incorrect+"</h3>");
+        $('#subWrapper').append("<h3>Unanswered:"+triviaQuestions.length-(this.incorrect +this.correct ))+ "</h3>");
+    }
+}
+// ...........................
+
+// $(document).ready(function () {
+
+//     // variables
+//     // start time, time remaining, questions, answers, correct answer, wins, losses, restart
+//     var currentQuestion = 0;   
+//     var correct = 0;
+//     var incorrect = 0;
+//     var unanswered = 0;
+//     var timeRemaining = 30;
+//     var gifArray = ["question1", "question2", "question3"],
+//     var message = {
+//         correct: "Correct Answer!",
+//         incorrect: "Incorrect Answer!",
+//         endTime: "Times Up!",
+//         finished: "Your Results",
+//     }
+//     var triviaQuestions = {
+//         question1 : {
+//             question: "Sesame Street was first aired on public braodcasting televsion on what date?",
+//             answerList: ["November 10, 1969","October 5, 1989", "March 27, 1954,"],
+//             answer: 0,
+//             answerText: "Sesame Street was first aired on public braodcasting televsion on Novemeber 10, 1969."
+//         },
+
+//          question2 : {
+//          question: "True or False: The cast of Sesame Street consist of puppets and human characters.",
+//          answerList: ["True","False"],
+//          answer: 0,
+//          answerText: "The cast of Sesame Street consist of puppets and human characters. True."
+//          },
+
+//          question3 : {
+//              question: "Who was the main protagonist of the series and the first Muppet to appear on the show?",
+//              answerList: ["Oscar the gouch","Big Bird","Elmo"],
+//              answer: 1,
+//              answerText: "Big Bird was the main protagonist of the series and the first Muppet to appear on the show."
+//          },
+//     }
+
+//     $('#startBtn').on('click', function () {
+//     $(this).hide();
+//     newGame();
+//     });
+
+
+//     $('#startOverBtn').on('click', function (){
+//         $(this).hide();
+//         newGame();
+//     });
+// function newGame(){
+// $("#game").show();
+// $("#correct").empty();
+// $("#incorrect").empty();
+// $("#unanswered").empty();
+// $("#gif").hide();
+// $("#gifCaption").hide();
+// currentQuestion
+// correct
+// incorrect
+// unanswered
+// newQuestion();
+
+// }
+// function newQuestion(){
+//     $("#message").empty();
+//     $("#correct").empty();
+//     $("#gif").hide();
+//     $("#gifCaption").hide();
+//     answer = true;
+
+// }
+//     $("#correct").html("Question" + (correct+1) + "of" + triviaQuestions.length);
+//   $(".question").html("<h2>" + triviaQuestions[currentQuestion].question + "</h2>");
+
+//   for(var i = 0; i < 2; i++) {
+// var choices = $("<div>");
+// choices.text(triviaQuestions[currentQuestion].answerList[i]);
+// 		choices.attr({'data-index': i });
+// 		choices.addClass('thisChoice');
+// 		$('.answerList').append(choices);
+//   }
+
+// });
 
 
 
+//   ........................
 
 // $(document).ready(function () {
 //     $("#start-button").on("click",gameState.startTimer);
